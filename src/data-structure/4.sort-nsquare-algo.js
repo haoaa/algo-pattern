7fa38ba976be9cb34d53a9d1d3b5d29062a91b15
郑建彬 <zhengjb@shanhs.com>
2019-11-25 09:57:21 +0800

    x

--- src/data-structure/4.sort-nsquare-algo.js
+++ src/data-structure/4.sort-nsquare-algo.js
@@ -125,11 +125,92 @@ class Sort{
     }
     mergeCall(arr, 0, arr.length - 1)
   }
+  quickSort(arr = []) {
+    let that = this
+    if (arr.length <= 1) return arr
+    function callQuick(arr, start, end) {
+      if (start >= end) {
+        return
+      }
+      let q = partition(arr, start, end)
+      callQuick(arr, start, q - 1) // 这里不再包含分区点
+      callQuick(arr, q + 1, end)
+    }
+    function partition(arr, start, end) {
+      let pivot = arr[end]
+      let sortedIndex = start
+      for (let i = start; i < end; i++) {
+        if (arr[i] < pivot) { // 不需要等号, 等于pivot就放在大于分区
+          that.swap(arr, sortedIndex, i)
+          sortedIndex++
+        }        
+      }
+      that.swap(arr, sortedIndex, end) //把分区点放入中间位置
+      return sortedIndex
+    }
+    callQuick(arr, 0, arr.length - 1)
+  }
+  /* 
+    找到第k大是数据如果是有序数组 序号i - 1就是第i大数据, 借由分区方法不需要完全排好序就可以找到k大数.
+    利用分区点已经是在完全排序后的序号上. 每次分区后就看分区点q + 1是不是大于k, 如果小于就说明要在大分区里继续找.
+  */
+  findKthNumber(arr = [], kth) {
+    let that = this
+    let result
+    function callFind(arr, start, end) {
+      if (start >= end) {
+        if(start + 1 === kth) {
+          result = arr[start] 
+        }
+        return
+      }
+      let q = partition(arr, start, end)
+      if(q + 1 === kth) {
+        result = arr[q]
+        return
+      }
+      if (q + 1 > kth) {
+        callFind(arr, start, q - 1)  
+      } else {
+        callFind(arr, q + 1, end)
+      }      
+    }
+    function partition(arr, start, end) {
+      let pivot = arr[end]
+      let sortedIndex = start
+      for (let i = start; i < end; i++) {
+        if (arr[i] < pivot) { // 不需要等号, 等于pivot就放在大于分区
+          that.swap(arr, sortedIndex, i)
+          sortedIndex++
+        }        
+      }
+      that.swap(arr, sortedIndex, end) //把分区点放入中间位置
+      return sortedIndex
+    }
+    function middlePivot(arr, start, end) {
+      // 三数取中
+      let mid =(start + end) / 2 | 0
+      let temp = [{index: start, value: arr[start]}, {index: end, value: arr[end]}, {index: mid, value: arr[mid]}]
+      temp.sort((a, b) => a.value - b.value)
+      return temp[1].index
+    }
+    function randomPivot(arr, start, end) {
+      let randomIndex = Math.floor(Math.random() * (end - start + 1)) + start
+      if (randomIndex !== end) {        
+        this.swap(arr, randomIndex, end)
+      }
+    }
+    callFind(arr, 0, arr.length - 1)
+    return result
+  }
 }
 
 let st = new Sort()
-let arr = [4, 5, 6, 3, 2, 1]
+let arr = [1, 1, 2]
 // st.bubleSort(arr)
 // st.mergeSort(arr)
-st.mergeSortWithSentinel(arr)
-console.log(arr);
+// st.mergeSortWithSentinel(arr)
+// st.quickSort(arr)
+console.log(st.findKthNumber(arr, 0));
+
+// console.log(arr);
