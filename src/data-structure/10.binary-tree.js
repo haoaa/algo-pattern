3ac9a6a1cec9f663cdf1fd7ab224a0607f1c46e4
郑建彬 <zhengjb@shanhs.com>
2019-12-08 14:38:18 +0800

    linked-hash-table

---
+++ src/data-structure/10.binary-tree.js
@@ -0,0 +1,16 @@
+class Node {
+  constructor(value) {
+    this.value = value
+    this.left = null
+    this.right = null
+  }
+}
+
+
+/* bst, search and traverse */
+class SearchTree {
+  constructor() {
+    this.root = null /* 根节点 */
+  }
+  
+}
--- src/data-structure/8.hash-table.js
+++ src/data-structure/8.hash-table.js
@@ -7,25 +7,38 @@ class Entry{
 }
 class HashTable{  
   constructor() {
-    this.table = new Array(HashTable.INITIAL_CAPACITY).fill(undefined)
+    this.table = new Array(HashTable.INITIAL_CAPACITY).fill(null)
     this.size = 0 // 元素个数
     this.use = 0 // 已使用槽(对应索引有数据)数目
   }
   /* 散列函数 */
-  hash(key) {
+  hashBad(key) {
     // 1. 散列函数计算得到的散列值是一个非负整数；
     // 2. 如果 key1 = key2，那 hash(key1) == hash(key2)；
-    // 3. 如果 key1 ≠ key2，那 hash(key1) ≠ hash(key2)。 (X)
+    // 3. 如果 key1 ≠ key2，那 hash(key1) ≠ hash(key2)。 (X) todo
     key = String(key)
     let code = key.charCodeAt(key.length - 1) % this.table.length
     return key === undefined ? 0 : code
+  }
+  hash(key) {
+    // 1. 散列函数计算得到的散列值是一个非负整数；
+    // 2. 如果 key1 = key2，那 hash(key1) == hash(key2)；
+    // 3. 如果 key1 ≠ key2，那 hash(key1) ≠ hash(key2)。 (X)依靠冲突处理逻辑(链表,开放寻址)
+    key = String(key)
+    let length = key.length
+    let hash = length
+    for (let i = 0; i < length; i++) {
+      hash = ((hash << 5) ^ (hash >> 27)) ^ key.charCodeAt(i)      
+    }
+    // a % b = a & (b - 1) (b%2 ==0)
+    return hash & (this.table.length - 1)
   }
   resize() {
     /* js要动态扩容吗, js数组在内存是连续存储吗 */
     let newTable = new Array(this.table.length * 2)
     for (let i = 0; i < this.table.length; i++) {
       const element = this.table[i];
-      if (element === undefined) {
+      if (element === null) {
         continue
       }
       newTable[i] = element
@@ -35,7 +48,7 @@ class HashTable{
   /* 插入 */
   put(key, value) {
     let index = this.hash(key)
-    if (this.table[index] === undefined) {
+    if (this.table[index] === null) {
       this.table[index] = new Entry(null, null, null) // 头节点为哨兵节点
     }
     let tmp = this.table[index]
@@ -71,7 +84,7 @@ class HashTable{
         tmp.next = next.next
         this.size--
         if (this.table[index].next === null) {
-          use--
+          this.use--
         }
         return next.value
       }
@@ -97,7 +110,7 @@ class HashTable{
     let ret = {}
     for (let i = 0; i < this.table.length; i++) {
       let element = this.table[i];
-      if (element!== undefined && element.next !== null) {
+      if (element!== null && element.next !== null) {
         ret[i] = []
         while (element.next !== null) {
           element = element.next
@@ -119,6 +132,8 @@ table.put('bd', 5)
 table.put('ba', 6)
 table.put('be', 7)
 // console.log(table.toString());
-console.log(table.remove('aa'));
+// console.log(table.remove('aa'));
 // console.log(table.toString());
-console.log(table.get('ba'));
+// console.log(table.get('ba'));
+
+module.exports= HashTable
