4bdad8f2fbe9f44c3025c99e39865540239a0a5f
浩齐 <290058005@qq.com>
2019-12-16 21:40:35 +0800

    kmp

--- src/data-structure/17.str-match-kmp.js
+++ src/data-structure/17.str-match-kmp.js
@@ -8,30 +8,50 @@ next01  0  0  1  2  3  0
 
 function genNext(sub) {
   let m = sub.length
-  let next = Array.from({length: m}).fill(-1)
-  let k = -1 //最长可匹配前缀子串的后缀子串长度
-  for (let i = 1; i < m; i++) {
-    while (k !== -1 && sub[k + 1] !== sub[i]) {
-      k = next[k]
-    }
-    if (sub[k + 1] === sub[i]) { // 前与后比
-      ++k
-    }
-    next[i] = k
+  let next = Array.from({length: m}).fill(0)
+  let j = 0, i = 1 
+  // 遍历后缀子串[1, m), 记录前缀子串的结束的下个字符下标, 同时尝试获取最大后缀子串.  
+  for (let j = 0; j < m && i < m;) {
+    if (sub[i] === sub[j]) {
+      next[i] = j + 1
+      i++
+      j++
+    } else {
+      if(j > 0) {
+        j = next[j - 1] // 获取失败, 尝试上一次匹配前缀子串的下一位
+      } else {        
+        i++
+      }
+    }    
   }
   return next
 }
 
-let a= 'ababacd'
-console.log(genNext(a));
-
+// function genNext(sub) {
+//   let m = sub.length
+//   let next = Array.from({length: m}).fill(0)
+//   let j = 0, i = 1 
+//   // 遍历后缀子串[1, m), 记录前缀子串的结束的下个字符下标, 同时尝试获取最大后缀子串.  
+//   while (i < m) {
+//     while(j > 0 && sub[i] !== sub[j]) {
+//       j = next[j - 1] // 获取失败, 尝试上一次匹配前缀子串的下一位
+//     }
+//     if (sub[i] === sub[j]) {
+//       next[i] = j + 1
+//       j++
+//     }    
+//     i++
+//   }
+//   return next
+// }
+// genNext('ababac')
 function kmp(src, substring) {
   let next = genNext(substring)
   let j = 0
   let m = substring.length
   for (let i = 0, n = src.length; i < n; i++) {
     while (j > 0 && src[i] !== substring[j]) {
-      j = next[j - 1] + 1
+      j = next[j - 1] 
     }
     if (src[i] === substring[j]) {
       j++
@@ -43,4 +63,4 @@ function kmp(src, substring) {
   return -1
 }
 
-console.log(kmp('abcabcaababacacdeft', 'ababac'));
+console.log(kmp('abxabcabcaby', 'abcaby'));
