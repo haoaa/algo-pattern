13571391ef1804ddece5cd1cf16157e7952cf7b2
haoqi <290058005@qq.com>
2024-12-07 14:16:11 +0800

    up promise

--- src/how-it-work/how-promise-work.js
+++ src/how-it-work/how-promise-work.js
@@ -1,194 +1,173 @@
-/* eslint-disable */ 
-const REJECTED = 'rejected'
-const PENDING = 'pending'
-const FULLFILLED = 'fullfilled'
+/* eslint-disable */
+const REJECTED = "rejected";
+const PENDING = "pending";
+const FULLFILLED = "fullfilled";
 
-function MyPromise(executor) {
-  this.state = PENDING
-  this.value = null
-  this.reason = null
-  this.onFullfilledArray = []
-  this.onRejectedArray = []
-
-  const resolve = value => {
-    if (value instanceof MyPromise) {
-      MyPromise.then(resolve, reject)
-    }
-    if (this.state === PENDING){
-      this.state = FULLFILLED
-      this.value = value
-      setTimeout(() => {
-        this.onFullfilledArray.forEach(fun => fun(value))
-      }, 0)
-    }
-  }
-  const reject = reason => {
-    if (this.state === PENDING){
-      this.state = REJECTED
-      this.reason = reason
-      setTimeout(() => {
-        this.onRejectedArray.forEach(fun => fun(reason))
-      }, 0)
-    }
-  }
-  try {
-    executor(resolve, reject)
-  } catch (e) {
-    reject(e)
-  }
-}
-
-MyPromise.prototype.then = function(onFullfilled, onRejected) {
-  onFullfilled = typeof onFullfilled === 'function' ? onFullfilled : data => data
-  onRejected = typeof onRejected === 'function' ? onRejected : err => {throw new Error(err)}
-  
-  let promise2
-  if (this.state === FULLFILLED) {
-    return promise2 = new MyPromise((resolve2, reject2) => {
-      setTimeout(() => {
-        try {
-          let result = onFullfilled(this.value)
-          resolvePromise(promise2, result, resolve2, reject2)
-        } catch (error) {
-          reject2(error)
-        }
-      })
-    })    
-  } else if (this.state === REJECTED) {
-    return promise2 = new MyPromise((resolve2, reject2) => {
-      setTimeout(() => {
-        try {
-          let result = onRejected(this.reason)
-          resolvePromise(promise2, result, resolve2, reject2)
-        } catch (error) {
-          reject2(error)
-        }
-      })
-    })
-  } else if (this.state === PENDING) {
-    promise2 = new MyPromise((resolve2, reject2) => {
-      this.onFullfilledArray.push(() => {
-        try {
-          let result = onFullfilled(this.value)
-          resolvePromise(promise2, result, resolve2, reject2)
-        } catch (error) {
-          reject2(error)
-        }
-      })
-      this.onRejectedArray.push(_ => {
-        try {
-          let result = onRejected(this.reason)
-          resolvePromise(promise2, result, resolve2, reject2)
-        } catch (error) {
-          reject2(error)
-        }
-      })
-    })    
-  }
-  return promise2
-}
-
-const resolvePromise = (promise2, result, resolve2, reject2) => {
-  if (result === promise2) {
-    reject2(new TypeError('circular reference 吱吱吱?'))
-  }
-  let consumed = false
-  let thenable
-  if (result instanceof MyPromise) {
-    if (result.state !== PENDING) {
-      result.then(resolve2, reject2)
-    } else {
-      result.then((data) => {
-        resolvePromise(promise2, data, resolve2, reject2)
-      }, reject2)
-    }
-    return
-  }
-
-  const isThenable = target => (typeof target === 'function' || typeof target === 'object') &&
-    target !== null
-
-  if (isThenable(result)) {
+class MyPromise {
+  state = PENDING;
+  value = null;
+  reason = null;
+  onFullfilledArray = [];
+  onRejectedArray = [];
+  constructor(executor) {
+    const resolve = (value) => {
+      if (this.state === PENDING) {
+        this.state = FULLFILLED;
+        this.value = value;
+        setTimeout(() => {
+          this.onFullfilledArray.forEach((fun) => fun());
+        });
+      }
+    };
+    const reject = (reason) => {
+      if (this.state === PENDING) {
+        this.state = REJECTED;
+        this.reason = reason;
+        setTimeout(() => {
+          this.onRejectedArray.forEach((fun) => fun());
+        });
+      }
+    };
     try {
-      thenable = result.then
-      if (typeof thenable === 'function') {
-        thenable.call(result, function(data) {
-          if (consumed) {
-            return
-          }
-          consumed = true
-          return resolvePromise(promise2, data, resolve2, reject2)
-        }, function(error) {
-          if (consumed) {
-            return
-          }
-          consumed = true
-          reject2(error)
-        })
-      } else {
-        resolve2(result)
-      }
-    } catch (error) {
-      if (consumed) {
-        return
-      }
-      consumed = true
-      return reject2(error)
+      executor(resolve, reject);
+    } catch (e) {
+      reject(e);
     }
-  } else {
-    resolve2(result)
+  }
+  then(onFullfilled, onRejected) {
+    onFullfilled =
+      typeof onFullfilled === "function" ? onFullfilled : (data) => data;
+    onRejected =
+      typeof onRejected === "function"
+        ? onRejected
+        : (err) => {
+            throw err;
+          };
+
+    let promise2;
+    promise2 = new MyPromise((resolve2, reject2) => {
+      if (this.state === FULLFILLED) {
+        setTimeout(() => {
+          try {
+            let result = onFullfilled(this.value);
+            this.resolvePromise(promise2, result, resolve2, reject2);
+          } catch (error) {
+            reject2(error);
+          }
+        });
+      } else if (this.state === REJECTED) {
+        setTimeout(() => {
+          try {
+            let result = onRejected(this.reason);
+            this.resolvePromise(promise2, result, resolve2, reject2);
+          } catch (error) {
+            reject2(error);
+          }
+        });
+      } else if (this.state === PENDING) {
+        this.onFullfilledArray.push(() => {
+          try {
+            let result = onFullfilled(this.value);
+            this.resolvePromise(promise2, result, resolve2, reject2);
+          } catch (error) {
+            reject2(error);
+          }
+        });
+        this.onRejectedArray.push(() => {
+          try {
+            let result = onRejected(this.reason);
+            this.resolvePromise(promise2, result, resolve2, reject2);
+          } catch (error) {
+            reject2(error);
+          }
+        });
+      }
+    });
+    return promise2;
+  }
+  resolvePromise(promise2, result, resolve2, reject2) {
+    if (result === promise2) {
+      reject2(new TypeError("circular reference"));
+      return;
+    }
+    let called = false; // 异构thenable
+    let thenable;
+
+    const isThenable = (res) =>
+      ["function", "object"].includes(typeof res) && res !== null;
+
+    if (isThenable(result)) {
+      try {
+        thenable = result.then; // 单次获取
+        if (typeof thenable === "function") {
+          thenable.call( // thenable是MyPromise
+            result,
+            (data) => {
+              if (called) {
+                return;
+              }
+              called = true;
+              return this.resolvePromise(promise2, data, resolve2, reject2);
+            },
+            (error) => {
+              if (called) {
+                return;
+              }
+              called = true;
+              reject2(error);
+            }
+          );
+        } else {
+          resolve2(result);
+        }
+      } catch (error) {
+        if (called) {
+          return;
+        }
+        called = true;
+        return reject2(error);
+      }
+    } else {
+      resolve2(result);
+    }
+  }
+  catch(onRejected) {
+    return this.then(null, onRejected);
+  }
+  static resolve(value) {
+    return new MyPromise((resolve, reject) => {
+      resolve(value);
+    });
+  }
+  static reject(value) {
+    return new MyPromise((resolve, reject) => {
+      reject(value);
+    });
   }
 }
+function test() {
+  let resolve, reject;
+  const adapter = {
+    resolved: MyPromise.resolve,
+    rejected: MyPromise.reject,
+    deferred() {
+      let promise = new MyPromise((re, rj) => {
+        reject = rj;
+        resolve = re;
+      });
+      return {
+        resolve,
+        reject,
+        promise,
+      };
+    },
+  };
+//   const promisesAplusTests = require("./lib/programmaticRunner");
+  const promisesAplusTests =  require("promises-aplus-tests");
 
-MyPromise.prototype.catch = function (onRejected) {
-    return this.then(null, onRejected)
+  promisesAplusTests(adapter, { reporter: "spec", bail: true }, function (err) {
+    console.log(err);
+  });
 }
-MyPromise.resolve = function (value) {
-  return new MyPromise((resolve, reject) => {
-    resolve(value)
-  })
-}
-// let pro = new MyPromise((res, rej) => {
-//   return new MyPromise((r,j) => r(332))
-// }).then(data => {
-//   console.log(data);
-// })
-
-// setTimeout(() => {
-//   pro.then(a => {
-//     console.log(a);
-//   })
-// })
-
-
-// const promise = new MyPromise((resolve, reject) => {
-//   resolve('lucas')
-//   // setTimeout(() => {
-//   //     resolve('lucas')
-//   // }, 2000)
-// })
-
-// promise.then(onfulfilled = data => {
-//   console.log(data)
-//   return onfulfilled(data)
-// })
-// .then(data => {
-//   console.log(data)
-// })
-const promise = new MyPromise((resolve, reject) => {
-  setTimeout(() => {
-      resolve('lucas')
-  }, 2000)
-})
-
-promise.then(data => {
-  console.log(data)
-  return new MyPromise((resolve, reject) => {
-    setTimeout(() => {
-        resolve(`${data} next then`)
-    }, 4000)
-  })
-})
-.then(data => {
-  console.log(data)
-})
+// test();
+ 
